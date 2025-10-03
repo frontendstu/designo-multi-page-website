a4b8b27d78aa61ef14f879b51950a3021e7dda5b
Stu Cowley <stucowley@outlook.com>
2025-10-03 11:24:41 +0930

    docs(script.js): add introductory comment

--- src/main.js
+++ src/main.js
@@ -1,1 +1,13 @@
 import './sass/style.scss';
+
+/* =============================================================================
+ * Designo Multi-Page Website Frontend Mentor Challenge
+ *
+ * Author: Stu Cowley (@frontendstu)
+ * Date: 03-10-2025
+ * Challenge URL:
+ * https://www.frontendmentor.io/challenges/designo-multipage-website-G48K6rfUT
+ *
+ * Description:
+ * This file contains the scripts for this challenge.
+ * ========================================================================== */
