3eba628e30842c4d7fc6845311f5f16323ceaa09
Stu Cowley <stucowley@outlook.com>
2026-01-14 14:21:38 +1030

    feat(main.js): add header navigation toggle

--- src/main.js
+++ src/main.js
@@ -1,1 +1,19 @@
 import './sass/style.scss';
+
+/* =============================================================================
+ * HEADER NAVIGATION TOGGLE
+ * -----------------------------------------------------------------------------
+ *
+ * Controls the mobile navigation menu by toggling visibility
+ *
+ * ========================================================================== */
+
+const nav = document.querySelector(".header-nav");
+const toggle = document.querySelector(".header-toggle");
+
+toggle.addEventListener("click", () => {
+    const isOpen = toggle.getAttribute("aria-expanded") === "true";
+
+    toggle.setAttribute("aria-expanded", String(!isOpen));
+    nav.setAttribute("data-visible", String(!isOpen));
+});
