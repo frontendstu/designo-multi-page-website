1f4c4e956ea6ae1bf1f2ceb02afbaf986dbd63b9
Stu Cowley <stucowley@outlook.com>
2026-01-23 16:00:12 +1030

    chore(vite.config.js): prevent inline SVG from being inline rendered in CSS background image

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,9 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
     base: '/designo-multi-page-website',
+    build: {
+        assetsInlineLimit: (filePath, buffer) => {
+            return filePath.endsWith('.svg') ? false : null;
+        }
+    }
 });
