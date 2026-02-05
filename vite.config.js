907ddc4126cc7d40ff36f4d7819dab7a91187a69
Stu Cowley <stucowley@outlook.com>
2026-02-05 17:22:15 +1030

    fix(vite-config.js): fix Vite base path trailing slash

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    base: '/designo-multi-page-website',
+    base: '/designo-multi-page-website/',
     build: {
         assetsInlineLimit: (filePath, buffer) => {
             return filePath.endsWith('.svg') ? false : null;
